56c77af6ae3076a20968d948b9c08ff33c1f8efc
amritsagoo91 <amritsagoo91@gmail.com>
2024-10-24 14:16:30 -0700

    2.19*: Data for countries, step 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,56 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 
-function Countries({ countries }) {
-  return (
-    <>
-      {
 
-        countries.length === 1
-          ? <div>
-            <h2>{countries[0].name.common}</h2>
-            <p>capital: {countries[0].capital}</p>
-            <p>Area: {countries[0].area}</p>
-            <h3>Languages</h3>
-            <ul>
-              {Object.entries(countries[0].languages).map(([key, value]) => (
-                <li key={key}>{value}</li>
-              ))}
-
-            </ul>
-            <img src={countries[0].flags.png} />
-          </div >
-          : countries.map(country => (
-            <li key={country.cca3}>{country.name.common}</li>
-          ))
-
-      }
-    </>
-  );
+function CountryDetails({ country }) {
+  if (!country || !country.name) {
+    return null
+  }
+  return <>
+    <h1>{country.name.common}</h1>
+    <li>{country.capital}</li>
+    <li>{country.area}</li>
+    <h3>languages</h3>
+    {
+      Object.entries(country.languages).map(([_, value]) => <li>{value}</li>)
+    }
+    <img src={country.flags.png} alt={country.flags.alt} style={{ border: "1px solid grey" }}
+    />
+  </>
 }
+
+function CountryList({ countries, onShowClick }) {
+  return <ul >
+    {
+      countries.map(country => <li key={country.name.common}>{country.name.common}{" "}<button onClick={() => onShowClick(country.name.common)}>show</button></li>)
+    }
+  </ul>
+}
+
+
+
+function Countries({ countries, onShowClick, selectCountry }) {
+
+  return <>
+    {
+      countries.length === 1
+        ? <CountryDetails country={countries[0]} />
+        : <CountryList countries={countries} onShowClick={onShowClick} />
+    }
+    <CountryDetails country={selectCountry} />
+  </>
+
+
+}
+
 
 function App() {
   const [name, setName] = useState('');
-  const [countries, setCountries] = useState(null);
+  const [countries, setCountries] = useState([]);
   const [filterCountries, setFilterCountries] = useState(countries);
+  const [selectCountry, setSelectCountry] = useState({});
   const [message, setMessage] = useState("too many countries");
+
 
   useEffect(() => {
     axios.get('https://studies.cs.helsinki.fi/restcountries/api/all').then(response => {
@@ -48,20 +65,31 @@ function App() {
       country.name.common.toLowerCase().includes(filterName.toLowerCase())
     );
     setFilterCountries(filteredCountries);
-  };
+    setSelectCountry(null)
+  }
+
+  const handleShow = (countryName) => {
+    const foundObjByName = filterCountries.find(country => country.name.common === countryName)
+    setSelectCountry(foundObjByName)
+  }
+
 
   return (
     <>
       <form>
         <input value={name} onChange={(e) => handleChange(e.target.value)} />
       </form>
-      {name === ''
-        ? null
-        : (filterCountries.length >= 10
-          ? <p>{message}</p>
-          : <ul style={{ "listStyleType": "none" }}><Countries countries={filterCountries} /></ul>)}
+      {
+        name === ''
+          ? null
+          : filterCountries.length >= 10
+            ? <p>{message}</p>
+            : <Countries countries={filterCountries} onShowClick={handleShow} selectCountry={selectCountry} />}
+
+
     </>
   );
 }
 
 export default App;
+
